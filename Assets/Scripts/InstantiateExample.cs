a0de106262da41ee70960d5ea41ec211b3afbcb7
Arsalan-Syed <arsalan.s96@gmail.com>
2020-02-16 17:07:11 +0000

    Editor script to instantiate multiple instances of a prefab at random

---
+++ Assets/Scripts/InstantiateExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class InstantiateExample : MonoBehaviour
+{
+    private static int quantity = 10;
+    private static string prefabName = "ball";
+
+    [MenuItem("Custom Actions/Instantiate objects randomly")]
+    public static void Instantiate() {
+        
+        for (int i = 0; i < quantity; i++) {
+            GameObject instance = Instantiate(Resources.Load(prefabName, typeof(GameObject))) as GameObject;
+            instance.transform.position = generateRandomPosition();
+        }
+    }
+
+    private static Vector3 generateRandomPosition() {
+        float x = Random.Range(-10.0f, 10.0f);
+        float y = 1;
+        float z = Random.Range(-10.0f, 10.0f);
+        return new Vector3(x, y, z);
+    }
+}
